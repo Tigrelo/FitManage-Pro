5bd4a77b6a6d6bb2661fef06c58f7cc4993cc0e1
Isaac Ferreira Rodrigues <isaacferreirabatera@hotmail.com>
2026-02-20 19:31:10 -0300

    chore: initial commit

---
+++ src/main/java/FitManage/Pro/FitManageProApplication.java
@@ -0,0 +1,13 @@
+package FitManage.Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitManageProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitManageProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FitManage/Pro/FitManageProApplicationTests.java
@@ -0,0 +1,13 @@
+package FitManage.Pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitManageProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

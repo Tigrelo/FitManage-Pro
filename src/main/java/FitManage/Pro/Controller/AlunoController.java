4c604188c463ce163a4ac17abd07c0089ca86ac6
Isaac Ferreira Rodrigues <isaacferreirabatera@hotmail.com>
2026-02-25 17:24:31 -0300

    chore: Docker

--- src/main/java/FitManage/Pro/Controller/AlunoController.java
+++ src/main/java/FitManage/Pro/Controller/AlunoController.java
@@ -1,4 +1,27 @@
 package FitManage.Pro.Controller;
 
+import FitManage.Pro.Model.Aluno;
+import FitManage.Pro.Service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/alunos")
+@CrossOrigin(origins = "*") // Permite que o Angular acesse a API
 public class AlunoController {
+
+    @Autowired
+    private AlunoService service;
+
+    @PostMapping
+    public Aluno criar(@RequestBody Aluno aluno) {
+        return service.salvar(aluno);
+    }
+
+    @GetMapping
+    public List<Aluno> listar() {
+        return service.listarTodos();
+    }
 }
--- src/main/java/FitManage/Pro/Service/AlunoService.java
+++ src/main/java/FitManage/Pro/Service/AlunoService.java
@@ -1,7 +1,22 @@
 package FitManage.Pro.Service;
 
+import FitManage.Pro.Model.Aluno;
+import FitManage.Pro.Repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-
-
+@Service
 public class AlunoService {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    public Aluno salvar(Aluno aluno) {
+        return repository.save(aluno);
+    }
+
+    public List<Aluno> listarTodos() {
+        return repository.findAll();
+    }
 }

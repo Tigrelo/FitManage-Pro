2f8fdd7a108e93526c0b5dffbd19581b64d9a85d
Isaac Ferreira Rodrigues <isaacferreirabatera@hotmail.com>
2026-03-09 18:31:52 -0300

    feat: adicionar

--- src/main/java/FitManage/Pro/Model/Treino.java
+++ src/main/java/FitManage/Pro/Model/Treino.java
@@ -1,5 +1,29 @@
 package FitManage.Pro.Model;
 
-public class Treino {
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Entity
+@Data
+@Table(name = "Treinos")
+public class Treino {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String objetivo;
+    private LocalDate dataCriacao = LocalDate.now();
+    private LocalDate dataVencimento;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @OneToMany(mappedBy = "treino", cascade = CascadeType.ALL)
+    private List<ItemTreino> itens;
 }
